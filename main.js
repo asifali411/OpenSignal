517bf718fc9b9151c6a803800e728515b81986c1
asifali411 <asifalipm411@gmail.com>
2026-02-06 19:40:59 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
+
+let win;
+
+const createWindow = () => {
+    win = new BrowserWindow();
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, '/public/index.html'),
+        protocol: "file",
+        slashes: true
+    }))
+
+    win.on('close', () => {
+        win = null;
+    })
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform !== "darwin"){
+        app.quit();
+    }
+})
+app.on("activate", () => {
+    if(win == null){
+        createWindow();
+    }
+})
